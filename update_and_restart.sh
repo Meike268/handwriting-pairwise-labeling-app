00cc2015c16a83dfeec4dbf1f61198f5686a77d5
Lukas <lukas.pieger@prontoweb.de>
2024-08-08 11:36:46 +0200

    fix: Remove that submodule-update line

--- update_and_restart.sh
+++ update_and_restart.sh
@@ -4,13 +4,6 @@ set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR" || exit 1
 
-cd ./handwriting-labeling-app-frontend
-git pull origin main
-cd "$SCRIPT_DIR"
-cd ./handwriting-labeling-app-backend
-git pull origin main
-cd "$SCRIPT_DIR"
-
 docker-compose build
 docker-compose down
 docker-compose up -d
