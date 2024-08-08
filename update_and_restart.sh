7a9270d990ba757acb50584a061c8784f8886e6c
Lukas Pieger <tadeusteewurst@gmail.com>
2024-08-08 16:32:14 +0200

    Add copying files from nextcloud

--- update_and_restart.sh
+++ update_and_restart.sh
@@ -5,6 +5,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR" || exit 1
 
 git submodule update
+$SCRIPT_DIR/_update_files.sh
 
 docker-compose build
 docker-compose down
