718830438255f151c76e542c4208b048d2db1b8f
Lukas <lukas.pieger@prontoweb.de>
2024-08-08 11:42:46 +0200

    fix: Reintroduce the line but without unnecessary recursive flag

--- update_and_restart.sh
+++ update_and_restart.sh
@@ -4,6 +4,8 @@ set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR" || exit 1
 
+git submodule update
+
 docker-compose build
 docker-compose down
 docker-compose up -d
