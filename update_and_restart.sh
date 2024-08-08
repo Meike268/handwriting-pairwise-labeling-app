e88ff5affd8a58ab1a3c7c22950ede16269b5ce6
Lukas <lukas.pieger@prontoweb.de>
2024-08-08 09:52:23 +0200

    fix: submodules not being updated

--- update_and_restart.sh
+++ update_and_restart.sh
@@ -3,7 +3,12 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR" || exit 1
 
-git submodule update --recursive
+cd ./handwriting-labeling-app-frontend
+git pull origin main
+cd "$SCRIPT_DIR"
+cd ./handwriting-labeling-app-backend
+git pull origin main
+cd "$SCRIPT_DIR"
 
 docker-compose build
 docker-compose down
