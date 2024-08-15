0e58e08e4b646824f5ea80e76f727864b4d1fb2b
Lukas <lukas.pieger@prontoweb.de>
2024-08-15 19:53:41 +0200

    fix: env-typo

--- provide-defaults.sh
+++ provide-defaults.sh
@@ -25,7 +25,7 @@ if [ ! -f "$CONFIG_FILE" ]; then
 "{
    \"samplesOrigin\": \"xai_sentences\",
    \"batchSize\": 10,
-   \"minExpertAnswerCount\": 1,
+   \"targetExpertAnswerCount\": 1,
    \"targetAnswerCount\": 3,
    \"prioritizedReferenceSentences\": [
      {
